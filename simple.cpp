4ddb8dab0417860358044557a9dc4f51bace2e79
Yugal Kaushik <yugalkaushik14@gmail.com>
2025-07-07 23:24:18 +0530

    Pointers

--- simple.cpp
+++ simple.cpp
@@ -1,8 +1,11 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
 int main()
 {
+    string data = "Yugal" ;
     cout << "Hello World!";
+    cout << &data << endl;
     return 0;
 }
